0bf0b8e6734194781a353c4dbde8584db6d00b5c
ReTomines <renatatr.concurso@gmail.com>
2025-03-28 16:50:41 -0300

    4º commit - parte 2/2

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return 'UserController@index';
+        $user = User::first();
+        
+        // Verifica se um usuário foi encontrado
+        if ($user) {
+            return view('admin.users.index', compact('user'));
+        } else {
+            // Retorna uma mensagem caso nenhum usuário seja encontrado
+            return 'Nenhum usuário encontrado.';
+        }
     }
 }
